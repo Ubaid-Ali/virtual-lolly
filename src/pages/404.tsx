6c8f6320d4f9b289b271d6a4148d865e0e801289
Ubaid Ali <syedubaidali2020@gmail.com>
2021-08-02 20:26:21 +0500

    install gatsby-plugin-react-svg

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const NotFound404 = () => {
+    return (
+        <div style={{textAlign: "center"}}>
+            Sorry Page Not Found!
+        </div>
+    )
+}
+
+export default NotFound404;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import lolly from "../images/lolly-image.svg"
+
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <img src={lolly} alt="lollypop" />
+    </div>
+  )
 }
