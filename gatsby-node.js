df4d87ad5af98a65f70fffbdfb2f73422f158414
Ubaid Ali <syedubaidali2020@gmail.com>
2022-02-21 21:24:26 +0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,15 +2,20 @@ const path = require("path")
 
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
-  const { response } = await graphql(`
-    query {
-      lollies {
-        fillLollyTop
-        fillLollyMiddle
-        fillLollyBottom
-      }
-    }
-  `)
+  // const { response } = await graphql(`
+  //   query {
+  //     hello
+  //     lollies {
+  //       fillLollyTop
+  //       fillLollyMiddle
+  //       fillLollyBottom
+  //       recipientName
+  //       message
+  //       sender
+  //       lollyPath
+  //     }
+  //   }
+  // `)
 
   // response.data.allContentfulBlogPost.edges.forEach(edge => {
   //     createPage({
@@ -22,10 +27,10 @@ exports.createPages = async ({ actions, graphql }) => {
   //     })
   // })
   // data ?
-  console.log(
-    `response.data ================================== `,
-    response.data
-  )
+  // console.log(
+  //   `response.data ================================== `,
+  //   response
+  // )
   //   : console.log(
   //       `response.data ================================== FAILED TO DATA IN GATSBY-NODE.JS`
   //     )
