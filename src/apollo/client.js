c4916ef0647e89db7ada010d391f49e1d2bbd43a
Ubaid Ali <syedubaidali2020@gmail.com>
2022-02-22 01:12:05 +0500

    change connection of the serverless function client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: '/.netlify/functions/makeLolly',
+    uri: '/.netlify/functions/hello',
     fetch,
   }),
   cache: new InMemoryCache()
