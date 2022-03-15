79a5fb675f5331734e9e69611de38246e499702c
Ubaid Ali <syedubaidali2020@gmail.com>
2022-03-16 00:05:57 +0500

    Update dynamic_Lolly.tsx

--- src/templates/dynamic_Lolly.tsx
+++ src/templates/dynamic_Lolly.tsx
@@ -23,7 +23,7 @@ export const query = gql`
 
 interface DynamicLollyProps {
   // coming from gatsby-node.js Build time page
-  pageContext: {
+  pageContext?: {
     lollyPath?: string;
   };
   // run time page
@@ -38,61 +38,63 @@ const DynamicLolly = (props: DynamicLollyProps) => {
     console.log('Client side Dynamic Page')
   }
 
-  const _lollyPath = props.pageContext?.lollyPath || props.lollyPath;
+  const _lollyPath = props?.pageContext?.lollyPath || props.lollyPath;
   const { data, loading, error } = useQuery(query, {
     variables: { lollyPath: _lollyPath },
   })
 
-  const {
-    fillLollyTop,
-    fillLollyMiddle,
-    fillLollyBottom,
-    message,
-    sender,
-    recipientName
-  } = data?.getLollyUsingPath || {};
-
+  if (loading) return <h3 className="loading" >Loading Please Wait..</h3>
   if (error) {
     console.log('error', error.message)
     navigate("/404")
-    return error.message
+    return <h3>{error.message}</h3>
   }
-  if (loading) return <h3 className="loading" >Please Wait..</h3>
-  return (
-    <div className="container">
-      <Header />
-      <div className="create-lolly-container">
-        <Lolly
-          className="create-lolly-lollipop"
-          fillLollyTop={fillLollyTop}
-          fillLollyMiddle={fillLollyMiddle}
-          fillLollyBottom={fillLollyBottom}
-        />
-        {/*  */}
-        <div className="lollyForm">
-          <p className="created-top-text">
-            Your lolly is freezing. Share it with this link:
-          </p>
-          <p className="created-link">https://virtual-lolly-fullstack.netlify.app/lollies/{_lollyPath}</p>
-          <div className="showLolly-created-details">
-            <span className="name">{recipientName}</span>
-            <span>{message}</span>
-            <span className="name">--{sender}</span>
+  if (data) {
+    const {
+      fillLollyTop,
+      fillLollyMiddle,
+      fillLollyBottom,
+      message,
+      sender,
+      recipientName
+    } = data?.getLollyUsingPath;
+    // console.log('data', data)
+    return (
+      <div className="container">
+        <Header />
+        <div className="create-lolly-container">
+          <Lolly
+            className="create-lolly-lollipop"
+            fillLollyTop={fillLollyTop}
+            fillLollyMiddle={fillLollyMiddle}
+            fillLollyBottom={fillLollyBottom}
+          />
+          {/*  */}
+          <div className="lollyForm">
+            <p className="created-top-text">
+              Your lolly is freezing. Share it with this link:
+            </p>
+            <p className="created-link">https://virtual-lolly-fullstack.netlify.app/lollies/{_lollyPath}</p>
+            <div className="showLolly-created-details">
+              <span className="name">{recipientName}</span>
+              <span>{message}</span>
+              <span className="name">--{sender}</span>
+            </div>
+            <p className="showLolly-created-last-text">
+              <span>{`${sender} `}</span>
+              made this virtual lollipop for you. You can
+              <Link to="/createNew" className="showLolly-created-last-text-link">
+                {" make your own "}
+              </Link>
+              to send to a friend who deserve some sugary treat which won't rot
+              their teeth...
+            </p>
           </div>
-          <p className="showLolly-created-last-text">
-            <span>{`${sender} `}</span>
-            made this virtual lollipop for you. You can
-            <Link to="/createNew" className="showLolly-created-last-text-link">
-              {" "}make your own{" "}
-            </Link>
-            to send to a friend who deserve some sugary treat which won't rot
-            their teeth...
-          </p>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 };
 
 
